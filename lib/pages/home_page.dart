24b8fd5bb96ad1369f88c23cea655ea322bd2d74
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2022-08-23 15:50:23 +0530

    c

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
-
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,22 +15,22 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final items = [
-      Icon(
+      const Icon(
         Icons.home,
         size: 30,
         color: Colors.black,
       ),
-      Icon(
+      const Icon(
         Icons.favorite,
         size: 30,
         color: Colors.black,
       ),
-      Icon(
+      const Icon(
         Icons.notifications,
         size: 30,
         color: Colors.black,
       ),
-      Icon(
+      const Icon(
         Icons.search,
         size: 30,
         color: Colors.black,
@@ -42,10 +42,10 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.transparent,
-        leading: Icon(Icons.menu),
+        leading: const Icon(Icons.menu),
         actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 20.0),
+          const Padding(
+            padding: EdgeInsets.only(right: 20.0),
             child: Icon(Icons.person),
           ),
         ],
@@ -62,8 +62,32 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         children: [
           // Find thr best coffee for you
-
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25.0),
+            child: Text(
+              'Find the best coffee for you',
+              style: GoogleFonts.bebasNeue(fontSize: 56),
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
           // Search bar
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                hintText: 'Find your coffee...',
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade600),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey.shade600),
+                ),
+              ),
+            ),
+          ),
 
           // horizontal listview of coffee tiles
         ],
